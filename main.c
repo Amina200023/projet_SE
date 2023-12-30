1fd3d98e9f471eb7673ddc800f5aa20760d0dbdc
aminaachouri <155119667+aminaachouri@users.noreply.github.com>
2023-12-30 18:10:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define N 10  // Taille du tampon
-#define n1 3  // Nombre de lignes de B
-#define m1 3  // Nombre de colonnes de B
-#define n2 3  // Nombre de lignes de C
-#define m2 3  // Nombre de colonnes de C
+#define N 10 
+#define n1 3  
+#define m1 3  
+#define n2 3 
+#define m2 3 
 
 // Matrices
 int B[n1][m1];
@@ -22,18 +22,18 @@ pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
 
-// Producteur
+
 void producer(void) {
     int item;
-    // Boucle pour chaque ligne de la matrice résultante A
+
     for (int i = 0; i < n1; ++i) {
         item = produce(i);
-        sem_wait(&empty);  // Attente d'une place libre
+        sem_wait(&empty);  
         pthread_mutex_lock(&mutex);
         // Section critique
         insert_item(i, item);
         pthread_mutex_unlock(&mutex);
-        sem_post(&full);  // Il y a une place remplie en plus
+        sem_post(&full);  
     }
 }
 
@@ -41,37 +41,35 @@ void producer(void) {
 void consumer(void) {
     int item;
     while (1) {
-        sem_wait(&full);  // Attente d'une place remplie
+        sem_wait(&full); 
         pthread_mutex_lock(&mutex);
         // Section critique
         item = remove_item();
         pthread_mutex_unlock(&mutex);
-        sem_post(&empty);  // Il y a une place libre en plus
+        sem_post(&empty);  
         consume(item);
     }
 }
 
-// Fonction principale
 int main() {
-    // Initialisation des matrices et du tampon (à compléter)
+ 
     initializeMatrices();
     initializeBuffer();
 
-    // Initialisation des mutex et sémaphores
+
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, N);
     sem_init(&full, 0, 0);
 
-    // Création des threads (à compléter)
     pthread_t producerThread, consumerThread;
     pthread_create(&producerThread, NULL, (void *)producer, NULL);
     pthread_create(&consumerThread, NULL, (void *)consumer, NULL);
 
-    // Attente des threads (à compléter)
+
     pthread_join(producerThread, NULL);
     pthread_join(consumerThread, NULL);
 
-    // Destruction des mutex et sémaphores
+  
     pthread_mutex_destroy(&mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
